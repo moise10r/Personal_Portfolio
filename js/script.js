cb60fd87ff37e05f60a23f841696308ce1c09f95
moise10r <moiserushanika2006@gmail.com>
2021-06-21 16:43:53 +0200

    Fix the mobile-menu on the desktop version.

--- js/script.js
+++ js/script.js
@@ -2,19 +2,17 @@ const menuButtonOpen = document.querySelector(".menu");
 const menuButtonClose = document.querySelector(".close");
 const header = document.querySelector("#header");
 
-var isOpen=false;
+let isOpen = false;
 
-
-menuButtonOpen.addEventListener('click',toggle);
-menuButtonClose.addEventListener('click', toggle);
+menuButtonOpen.addEventListener("click", toggle);
+menuButtonClose.addEventListener("click", toggle);
 
 function toggle() {
-  if(!isOpen){
-    header.classList.add("open");
-    isOpen=true;
-  }
-  else{
-    header.classList.remove("open");
-    isOpen = false;
-  }
+	if (!isOpen) {
+		header.classList.add("open");
+		isOpen = true;
+	} else {
+		header.classList.remove("open");
+		isOpen = false;
+	}
 }
