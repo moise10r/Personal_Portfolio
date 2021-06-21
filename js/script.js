ac19f5856ce351c53b938d0fe0166ffa1b972b9c
calvnce <intels_arcade@outlook.com>
2021-06-21 17:34:22 +0300

    Add mobile menu responsive button

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const menuButtonOpen = document.querySelector(".menu");
+const menuButtonClose = document.querySelector(".close");
+const header = document.querySelector("#header");
+
+var isOpen=false;
+
+
+menuButtonOpen.addEventListener('click',toggle);
+menuButtonClose.addEventListener('click', toggle);
+
+function toggle() {
+  if(!isOpen){
+    header.classList.add("open");
+    isOpen=true;
+  }
+  else{
+    header.classList.remove("open");
+    isOpen = false;
+  }
+}
