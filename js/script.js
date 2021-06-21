eab752c12af235353782810c61dfc4ac1158e071
moise10r <moiserushanika2006@gmail.com>
2021-06-21 20:34:35 +0200

    Add eslint linter config.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,17 @@
-const menuButtonOpen = document.querySelector(".menu");
-const menuButtonClose = document.querySelector(".close");
-const header = document.querySelector("#header");
+const menuButtonOpen = document.querySelector('.menu');
+const menuButtonClose = document.querySelector('.close');
+const header = document.querySelector('header');
 
 let isOpen = false;
 
-menuButtonOpen.addEventListener("click", toggle);
-menuButtonClose.addEventListener("click", toggle);
-
 function toggle() {
-	if (!isOpen) {
-		header.classList.add("open");
-		isOpen = true;
-	} else {
-		header.classList.remove("open");
-		isOpen = false;
-	}
+  if (!isOpen) {
+    header.classList.add('open');
+    isOpen = true;
+  } else {
+    header.classList.remove('open');
+    isOpen = false;
+  }
 }
+menuButtonOpen.addEventListener('click', toggle);
+menuButtonClose.addEventListener('click', toggle);
