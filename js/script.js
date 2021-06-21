d3a59c57324b90a598e725eb45615d438e0cd19f
moise10r <moiserushanika2006@gmail.com>
2021-06-21 21:04:10 +0200

    Add blur effect on the healine section.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
 const menuButtonOpen = document.querySelector('.menu');
 const menuButtonClose = document.querySelector('.close');
 const header = document.querySelector('header');
+const headline = document.querySelector('.headline');
 
 let isOpen = false;
 
 function toggle() {
   if (!isOpen) {
     header.classList.add('open');
+    headline.classList.add('bur-effect');
     isOpen = true;
   } else {
     header.classList.remove('open');
+    headline.classList.remove('bur-effect');
     isOpen = false;
   }
 }
