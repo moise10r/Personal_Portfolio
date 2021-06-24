8549c19ac3e195ff89cac3e8de4563232875ae05
moise10r <moiserushanika2006@gmail.com>
2021-06-24 20:08:30 +0200

    Fix popup window style.

--- js/data.js
+++ js/data.js
@@ -1,27 +1,5 @@
 const data = [
   {
-    id: 1,
-    image: 'images/Portfolio_1.png',
-    name: 'Tonic',
-    description: {
-      type: 'CANOPY',
-      interface: 'Backend Dev',
-      year: 2015,
-    },
-    fullDesciption: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae eius quae optio et quas, quibusdam quis excepturi omnis? Quos, officia accusamus. Aperiam, molestiae beatae numquam vitae modi est quod laboriosam! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi voluptatum quibusdam adipisci voluptates magnam ipsam dolores ad! Adipisci veritatis, sint perspiciatis reiciendis omnis, maiores incidunt, ex aut inventore deleniti nemo. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil mollitia deleniti nobis facilis delectus consequuntur, culpa commodi quia repellendus molestias doloribus praesentium voluptas nisi sint, similique repellat atque maiores aperiam.    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita ex quam officia ea dolor. Ea quasi in id dolorum ',
-    detail: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    technologies: [
-      'Html',
-      'Css',
-      'Javascript',
-      'github',
-      'Ruby',
-      'Bootstrap',
-    ],
-    liveDemo: 'https://moise10r.github.io/Personal_Portfolio/',
-    sourceCode: 'https://github.com/moise10r/Personal_Portfolio',
-  },
-  {
     id: 2,
     image: 'images/Portfolio_2.png',
     name: 'Multi-Post Stories',
@@ -76,6 +54,28 @@ const data = [
     },
     fullDesciption: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
     detail: 'Exploring the future of media in Facebooks first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
+    technologies: [
+      'Html',
+      'Css',
+      'Javascript',
+      'github',
+      'Ruby',
+      'Bootstrap',
+    ],
+    liveDemo: 'https://moise10r.github.io/Personal_Portfolio/',
+    sourceCode: 'https://github.com/moise10r/Personal_Portfolio',
+  },
+  {
+    id: 1,
+    image: 'images/Portfolio_1.png',
+    name: 'Tonic',
+    description: {
+      type: 'CANOPY',
+      interface: 'Backend Dev',
+      year: 2015,
+    },
+    fullDesciption: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae eius quae optio et quas, quibusdam quis excepturi omnis? Quos, officia accusamus. Aperiam, molestiae beatae numquam vitae modi est quod laboriosam! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi voluptatum quibusdam adipisci voluptates magnam ipsam dolores ad! Adipisci veritatis, sint perspiciatis reiciendis omnis, maiores incidunt, ex aut inventore deleniti nemo. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil mollitia deleniti nobis facilis delectus consequuntur, culpa commodi quia repellendus molestias doloribus praesentium voluptas nisi sint, similique repellat atque maiores aperiam.    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita ex quam officia ea dolor. Ea quasi in id dolorum ',
+    detail: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     technologies: [
       'Html',
       'Css',
