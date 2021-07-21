835da0c088b48143d009dbe371401fe25aa8d793
moise10r <moiserushanika2006@gmail.com>
2021-07-21 14:43:40 +0200

    Add the description of the project.

--- js/data.js
+++ js/data.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-tabs */
 const data = [
   {
     id: 2,
@@ -8,14 +9,13 @@ const data = [
       interface: 'Frontend Dev',
       year: 2015,
     },
-    fullDesciption: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-    detail: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
-    technologies: [
-      'Html',
-      'Css',
-      'Javascript',
-    ],
-    liveDemo: 'https://moise10r.github.io/Capstone_Project_Module_One/index.html',
+    fullDesciption:
+			'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    detail:
+			'Adoption is not simply a series of unconnected stories, but is in fact a movement that can change the lives of millions of children.',
+    technologies: ['Html', 'Css', 'Javascript'],
+    liveDemo:
+			'https://moise10r.github.io/Capstone_Project_Module_One/index.html',
     sourceCode: 'https://github.com/moise10r/Capstone_Project_Module_One',
   },
   {
@@ -27,16 +27,11 @@ const data = [
       interface: 'Backend Dev',
       year: 2015,
     },
-    fullDesciption: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-    detail: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
-    technologies: [
-      'Html',
-      'Css',
-      'Javascript',
-      'github',
-      'Ruby',
-      'Bootstrap',
-    ],
+    fullDesciption:
+			'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    detail:
+			'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+    technologies: ['Html', 'Css', 'Javascript', 'github', 'Ruby', 'Bootstrap'],
     liveDemo: 'https://moise10r.github.io/Personal_Portfolio/',
     sourceCode: 'https://github.com/moise10r/Personal_Portfolio',
   },
@@ -49,16 +44,11 @@ const data = [
       interface: 'Backend Dev',
       year: 2015,
     },
-    fullDesciption: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-    detail: 'Exploring the future of media in Facebooks first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
-    technologies: [
-      'Html',
-      'Css',
-      'Javascript',
-      'github',
-      'Ruby',
-      'Bootstrap',
-    ],
+    fullDesciption:
+			'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    detail:
+			'Exploring the future of media in Facebooks first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
+    technologies: ['Html', 'Css', 'Javascript', 'github', 'Ruby', 'Bootstrap'],
     liveDemo: 'https://moise10r.github.io/Personal_Portfolio/',
     sourceCode: 'https://github.com/moise10r/Personal_Portfolio',
   },
@@ -71,16 +61,11 @@ const data = [
       interface: 'Backend Dev',
       year: 2015,
     },
-    fullDesciption: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae eius quae optio et quas, quibusdam quis excepturi omnis? Quos, officia accusamus. Aperiam, molestiae beatae numquam vitae modi est quod laboriosam! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi voluptatum quibusdam adipisci voluptates magnam ipsam dolores ad! Adipisci veritatis, sint perspiciatis reiciendis omnis, maiores incidunt, ex aut inventore deleniti nemo. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil mollitia deleniti nobis facilis delectus consequuntur, culpa commodi quia repellendus molestias doloribus praesentium voluptas nisi sint, similique repellat atque maiores aperiam.    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita ex quam officia ea dolor. Ea quasi in id dolorum ',
-    detail: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    technologies: [
-      'Html',
-      'Css',
-      'Javascript',
-      'github',
-      'Ruby',
-      'Bootstrap',
-    ],
+    fullDesciption:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae eius quae optio et quas, quibusdam quis excepturi omnis? Quos, officia accusamus. Aperiam, molestiae beatae numquam vitae modi est quod laboriosam! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi voluptatum quibusdam adipisci voluptates magnam ipsam dolores ad! Adipisci veritatis, sint perspiciatis reiciendis omnis, maiores incidunt, ex aut inventore deleniti nemo. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil mollitia deleniti nobis facilis delectus consequuntur, culpa commodi quia repellendus molestias doloribus praesentium voluptas nisi sint, similique repellat atque maiores aperiam.    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita ex quam officia ea dolor. Ea quasi in id dolorum ',
+    detail:
+			'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    technologies: ['Html', 'Css', 'Javascript', 'github', 'Ruby', 'Bootstrap'],
     liveDemo: 'https://moise10r.github.io/Personal_Portfolio/',
     sourceCode: 'https://github.com/moise10r/Personal_Portfolio',
   },
